cb5678d1b9d3e561b6cbad600c0549c20195a5d7
pichifer3389 <63804024+pichifer3389@users.noreply.github.com>
2021-08-16 10:00:42 -0600

    Add files via upload

---
+++ includes/getLocalidad.php
@@ -0,0 +1,13 @@
+<?php
+	require ('../dbconfig.php');
+	
+	$id_municipio = $_POST['id_municipio'];
+	
+	$query = "SELECT id_localidad, localidad FROM t_localidad WHERE id_municipio = '$id_municipio' ORDER BY localidad";
+	$resultado=$conn->query($query);
+	
+	while($row = $resultado->fetch_assoc())
+	{
+		$html.= "<option value='".$row['id_localidad']."'>".$row['localidad']."</option>";
+	}
+	echo $html;
---
+++ includes/getMunicipio.php
@@ -0,0 +1,18 @@
+<?php
+	
+	require ('../dbconfig.php');
+	
+	$id_estado = $_POST['id_estado'];
+	
+	$queryM = "SELECT id_municipio, municipio FROM t_municipio WHERE id_estado = '$id_estado' ORDER BY municipio";
+	$resultadoM = $conn->query($queryM);
+	
+	$html= "<option value='0'>Seleccionar Municipio</option>";
+	
+	while($rowM = $resultadoM->fetch_assoc())
+	{
+		$html.= "<option value='".$rowM['id_municipio']."'>".$rowM['municipio']."</option>";
+	}
+	
+	echo $html;
+		
